0b8e5982b2521448a1459baea633344c8e6b9ef5
Petr Doležal <petr.dolezal@matfyz.cz>
2015-12-25 12:54:54 +0100

    Update to new release model
    Alas, Maven Repository Plugin is disappointing: when used in the simple
    way with Maven GPG Plugin as we did, it repackages some artifacts after
    signing them, which breaks the signatures.
    
    There is probably no easy solution and using Maven Release Plugin is a
    big burden at this moment: seriously, spending more time on dealing with
    these annoyances than with actual code development :-(
    
    Simple solution: make all as local as possible and make the release
    bundles with a script. It is not so elegant, definitely not as automated,
    but it work without much fuss and it could help to think twice before
    releasing a thing.

---
+++ scripts/make-release-bundle.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Creates a bundle for releasing
+
+if [ -z "$1" ]; then
+    echo "Syntax: $0 BASENAME"
+    exit 1
+fi
+
+if [ -n "$2" ]; then
+    cd "$2"
+fi
+
+BASENAME="$1"
+
+cp -f "$BASENAME.pom" "pom.xml"
+jar cf "$BASENAME-bundle.jar" "$BASENAME.jar"           \
+    "$BASENAME.jar" "$BASENAME.jar.asc"                 \
+    "$BASENAME-sources.jar" "$BASENAME-sources.jar.asc" \
+    "$BASENAME-javadoc.jar" "$BASENAME-javadoc.jar.asc" \
+    "pom.xml" "$BASENAME.pom.asc"
